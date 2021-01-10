f574ddda56c838eaac48eabe8ab3b6d9b0eff065
dialectic4th <hiromuber@gmail.com>
2021-01-10 22:11:50 +0900

    modify for local env

---
+++ module-2/02run_ecs.sh
@@ -0,0 +1,8 @@
+echo "write closter name and log group name"
+echo "exmaples is MythicalMysfits-Cluster mythicalmysfits-logs"
+echo "CLUSTER-NAME" $1
+echo "LOG-GROUP-NAME" $2
+ 
+aws ecs create-cluster --cluster-name $1
+aws logs create-log-group --log-group-name $2
+aws ecs register-task-definition --cli-input-json file://$PWD/aws-cli/task-definition.json
