2909a4ff8a68f5c19df1b8156bd5ec4362f129d7
dialectic4th <hiromuber@gmail.com>
2021-01-10 21:20:59 +0900

    add shell for cloudformation

---
+++ module-2/00run_cloudformation.sh
@@ -0,0 +1,6 @@
+echo "write stack name"
+echo "stack name exmaple is MythicalMysfitsCoreStack"
+echo "STACK-NAME" $1
+aws cloudformation create-stack --stack-name $1 --capabilities CAPABILITY_NAMED_IAM --template-body file://$PWD/cfn/core.yml   
+aws cloudformation describe-stacks --stack-name $1
+aws cloudformation describe-stacks --stack-name $1  > $PWD/../outputs/cloudformation-core-output.json
