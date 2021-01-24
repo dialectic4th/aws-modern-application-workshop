8b524f54e115cdaea0779b059a3cff6147ec0bcc
dialectic4th <hiromuber@gmail.com>
2021-01-24 16:42:09 +0900

    format

--- module-2/scripts/01run_container.sh
+++ module-2/scripts/01run_container.sh
@@ -9,7 +9,7 @@ docker build ./app/. -t $1.dkr.ecr.us-west-2.amazonaws.com/$2:$3
 # docker run -p 8080:8080 $1.dkr.ecr.us-west-2.amazonaws.com/$2:$3
 # curl http://0.0.0.0:8080/ 
 # {"message":"Nothing here, used for health check. Try /mysfits instead."}
-aws ecr create-repository --repository-name $2
+#aws ecr create-repository --repository-name $2
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $1.dkr.ecr.us-west-2.amazonaws.com
 docker push $1.dkr.ecr.us-west-2.amazonaws.com/$2:$3
 aws ecr describe-images --repository-name $2
--- module-2/scripts/02run_ecs.sh
+++ module-2/scripts/02run_ecs.sh
@@ -2,7 +2,6 @@ echo "write closter name and log group name"
 echo "exmaples is MythicalMysfits-Cluster mythicalmysfits-logs"
 echo "CLUSTER-NAME" $1
 echo "LOG-GROUP-NAME" $2
- 
 aws ecs create-cluster --cluster-name $1
 aws logs create-log-group --log-group-name $2
-aws ecs register-task-definition --cli-input-json file://$PWD/aws-cli/task-definition.json
+aws ecs register-task-definition --cli-input-json file://$PWD/aws-cli/task-definition.json > $PWD/../outputs/ecs-task-definition-output.json
