e061a11730bc445e30b566ebfd2d583f1d9f84b9
dialectic4th <hiromuber@gmail.com>
2021-01-24 19:36:06 +0900

    modify description

--- module-2/scripts/05run_html.sh
+++ module-2/scripts/05run_html.sh
@@ -1,4 +1,4 @@
-echo "write bucket name"
+echo "edit web/index.html and dewrite bucket name"
 echo "exmaple is mythical-mysfits-python-practice"
 aws s3 cp $PWD/web/index.html s3://$1/index.html
 open http://$1.s3-website-us-west-2.amazonaws.com
