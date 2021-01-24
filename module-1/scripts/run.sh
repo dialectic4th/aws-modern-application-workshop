72d7eef5e99f9f3b3070c2abe96a6750b39215a8
dialectic4th <hiromuber@gmail.com>
2021-01-24 21:57:34 +0900

    modify arg

--- module-1/scripts/run.sh
+++ module-1/scripts/run.sh
@@ -1,8 +1,8 @@
 echo "write bucket name to website-bucket-policy.json and argument when running"
-echo "bucket name example is mythical-mysfits-python-practice"
+echo "bucket name example is mythical-mysfits-python-practice-accountid"
 echo "REPLACE_ME_BUCKET_NAME" $1
 aws s3 mb s3://$1
 aws s3 website s3://$1 --index-document index.html
 aws s3api put-bucket-policy --bucket $1 --policy file://$PWD/aws-cli/website-bucket-policy.json
-aws s3 cp web/index.html s3://$1/index.html
-open http://$1.s3-website-us-west-2.amazonaws.com
+aws s3 cp web/index.html s3://mythical-mysfits-python-practice/index.html
+open http://mythical-mysfits-python-practice.s3-website-us-west-2.amazonaws.com
